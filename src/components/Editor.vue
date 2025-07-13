ab416c70ccabc7693a800955b312eb8b7fe41478
Saiful Alam Rakib <msa4rakib@gmail.com>
2025-07-13 12:45:51 +0600

    Implement dark mode functionality with theme toggle; refactor StatusBar and Editor components for improved layout and styling

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -17,6 +17,6 @@ watch(value, (newValue) => {
 
 <template>
     <textarea v-model="value" id="editor"
-        class="editor w-full h-screen p-4 pb-7 text-lg bg-neutral-100 dark:bg-neutral-900 text-black dark:text-white border-none outline-none resize-none font-mono placeholder:text-neutral-400 dark:placeholder:text-neutral-600"
+        class="editor w-full h-screen p-4 pb-7 text-base bg-neutral-100 dark:bg-neutral-900 text-black dark:text-white border-none outline-none resize-none font-mono placeholder:text-neutral-400 dark:placeholder:text-neutral-600"
         :class="classes" placeholder="Type your text here...&#10;&#10;or press `cmd + o` to open file." />
 </template>
--- src/components/StatusBar.vue
+++ src/components/StatusBar.vue
@@ -1,41 +1,31 @@
 <script setup lang="ts">
-import { ref } from 'vue';
 import Time from './Time.vue';
+import ThemeToggle from './ThemeToggle.vue';
 
 const {value}=defineProps<{
   value: string;
 }>();
-
-const darkTheme = ref(document.documentElement.classList.contains('dark'));
-
-const toggleTheme = () => {
-    document.documentElement.classList.contains('dark') ? document.documentElement.classList.remove('dark') : document.documentElement.classList.add('dark');
-
-    darkTheme.value = document.documentElement.classList.contains('dark');
-};
 </script>
 
 <template>
     <div class="absolute bottom-0 left-0 w-full max-h-7">
         <div
-            class="flex items-center justify-between w-full bg-neutral-200 dark:bg-neutral-800 text-neutral-800 dark:text-neutral-200 px-2 py-1 border-t border-neutral-300 dark:border-neutral-700 text-xs">
-
-            <!-- add a gap separator -->
+            class="flex items-center justify-start md:justify-between w-full bg-neutral-200 dark:bg-neutral-800 text-neutral-800 dark:text-neutral-200 px-2 py-1 border-t border-neutral-300 dark:border-neutral-700 text-xs">
             <div
-                class="flex justify-start items-center *:not-[:last-child]:after:content-['|'] *:not-[:last-child]:after:px-1">
-                <span>Character: {{ value.length }}</span>
-                <span>Word: {{ value.trim().split(/\s+/).filter(Boolean).length }}</span>
-                <span>Line: {{ value.split('\n').length }}</span>
+                class="hidden md:flex justify-start items-center *:not-[:last-child]:after:content-['|'] *:not-[:last-child]:after:px-1">
+                <span>Chars: {{ value.length }}</span>
+                <span>Words: {{ value.trim().split(/\s+/).filter(Boolean).length
+                    }}</span>
+                <span>Lines: {{ value.split('\n').length }}</span>
             </div>
 
-            <div
-                class="justify-end items-center hidden md:flex *:not-[:last-child]:after:content-['|'] *:not-[:last-child]:after:px-1">
-                <span>Mode: <span class="cursor-pointer" @click="toggleTheme" title="Click to switch theme">{{ darkTheme
-                        ? 'Dark' :
-                        'Light' }}</span></span>
-                <span class="flex items-center gap-2">Time: <Time /></span>
+            <div class="flex flex-1 md:justify-end justify-between items-center gap-3">
+                <ThemeToggle />
+                <div class="flex items-center gap-2">
+                    <span class="hidden md:flex">Time:</span>
+                    <Time />
+                </div>
             </div>
         </div>
     </div>
-
 </template>
