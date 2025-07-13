840db4438943d6fa492d5413395c2ab163380e43
Saiful Alam Rakib <msa4rakib@gmail.com>
2025-07-13 14:27:05 +0600

    Enhance Editor component with input shortcut handling; refactor event listeners for improved functionality

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -1,7 +1,10 @@
 <script setup lang="ts">
-import { ref, watch } from 'vue';
+import { onMounted, onUnmounted, ref, watch } from 'vue';
 import { useLanguageClass } from '../lib/use-language-class';
 import { useStorage } from '@vueuse/core';
+import { inputShortcutHandler } from '../lib/utils';
+
+const inputRef = ref<HTMLTextAreaElement | null>(null);
 
 const value = defineModel<string>({
     default: '',
@@ -15,16 +18,29 @@ watch(value, (newValue) => {
     classes.value = useLanguageClass(newValue);
 });
 
+onMounted(() => {
+    if (inputRef.value) {
+        inputRef.value.addEventListener('keydown', inputShortcutHandler(inputRef));
+    }
+});
+
+// Clean up event listener on unmount
+onUnmounted(() => {
+    if (inputRef.value) {
+        inputRef.value.removeEventListener('keydown', inputShortcutHandler(inputRef));
+    }
+});
+
 const settings = useStorage("settings", {
     fontSize: "16px",
 });
 
+
+const placeholder = "Start writing...\n\nPress `cmd + o` to open file.\nPress `cmd + ,` to open settings dialog.\n\nYou can also drag and drop files here.\n\nPress `cmd + b` for bold, `cmd + i` for italic, `cmd + u` for underline, and `cmd + k` for link.";
 </script>
 
 <template>
-    <textarea v-model="value" id="editor" autofocus
+    <textarea ref="inputRef" v-model="value" id="editor" autofocus
         class="editor w-full h-screen p-4 pb-7 text-base bg-neutral-100 dark:bg-neutral-900 text-black dark:text-white border-none outline-none resize-none font-mono placeholder:text-neutral-400 dark:placeholder:text-neutral-600"
-        :class="classes"
-        placeholder="Start writing...&#10;&#10;press `cmd + o` to open file.&#10;press `cmd + ,` to open settings dialog.&#10;&#10;You can also drag and drop files here."
-        :style="{ fontSize: `${settings.fontSize}px` }" />
+        :class="classes" :placeholder="placeholder" :style="{ fontSize: `${settings.fontSize}px` }" />
 </template>
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,5 @@
+import type { Ref } from "vue";
+
 export const formatDate = (date: Date): string => {
     // expected format: "Wed, 03 Jul, 12:00 AM"
     const options: Intl.DateTimeFormatOptions = {
@@ -10,3 +12,66 @@ export const formatDate = (date: Date): string => {
     };
     return date.toLocaleDateString("en-US", options);
 };
+
+export const inputShortcutHandler =
+    (inputRef: Ref<HTMLTextAreaElement | null>) => (event: KeyboardEvent) => {
+        if (!inputRef.value) return;
+
+        // Tab key for indentation
+        if (event.key === "Tab") {
+            event.preventDefault();
+            const start = inputRef.value?.selectionStart || 0;
+            const end = inputRef.value?.selectionEnd || 0;
+            const value = inputRef.value?.value || "";
+            inputRef.value!.value =
+                value.substring(0, start) + "\t" + value.substring(end);
+            inputRef.value!.selectionStart = inputRef.value!.selectionEnd =
+                start + 1;
+        }
+        // Ctrl + B for bold
+        if ((event.metaKey || event.ctrlKey) && event.key === "b") {
+            event.preventDefault();
+            const start = inputRef.value?.selectionStart || 0;
+            const end = inputRef.value?.selectionEnd || 0;
+            const value = inputRef.value?.value || "";
+            inputRef.value!.value =
+                value.substring(0, start) + "**" + value.substring(end);
+            inputRef.value!.selectionStart = inputRef.value!.selectionEnd =
+                start + 2;
+        }
+        // Ctrl + I for italic
+        if ((event.metaKey || event.ctrlKey) && event.key === "i") {
+            event.preventDefault();
+            const start = inputRef.value?.selectionStart || 0;
+            const end = inputRef.value?.selectionEnd || 0;
+            const value = inputRef.value?.value || "";
+            inputRef.value!.value =
+                value.substring(0, start) + "*" + value.substring(end);
+            inputRef.value!.selectionStart = inputRef.value!.selectionEnd =
+                start + 1;
+        }
+        // Ctrl + K for link
+        if ((event.metaKey || event.ctrlKey) && event.key === "k") {
+            event.preventDefault();
+            const start = inputRef.value?.selectionStart || 0;
+            const end = inputRef.value?.selectionEnd || 0;
+            const value = inputRef.value?.value || "";
+            inputRef.value!.value =
+                value.substring(0, start) +
+                "[Text](http://)" +
+                value.substring(end);
+            inputRef.value!.selectionStart = inputRef.value!.selectionEnd =
+                start + 14; // Adjust for "Link Text" length
+        }
+        // Ctrl + U for underline
+        if ((event.metaKey || event.ctrlKey) && event.key === "u") {
+            event.preventDefault();
+            const start = inputRef.value?.selectionStart || 0;
+            const end = inputRef.value?.selectionEnd || 0;
+            const value = inputRef.value?.value || "";
+            inputRef.value!.value =
+                value.substring(0, start) + "__" + value.substring(end);
+            inputRef.value!.selectionStart = inputRef.value!.selectionEnd =
+                start + 2;
+        }
+    };
